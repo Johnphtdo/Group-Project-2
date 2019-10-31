0163b999e2a3acbd13d6a32f337174cefa27690a
Dohn Joe <johnphtdo@gmail.com>
2019-10-31 10:34:53 -0500

    added routes to get recipes, and to post

---
+++ routes/api-routes.js
@@ -0,0 +1,44 @@
+// Dependencies
+var bcrypt = require("bcrypt");
+// Requiring our recipe models
+var db = require("../models");
+
+// Routes
+
+module.exports = function(app){
+
+    // GET route for getting all recipes by User
+    app.get("/api/recipes/:user_name", function(req, res){
+        db.Recipe.findAll({
+            where: {
+                user_name: req.params.user_name
+            }
+        }).then(function(data){
+            res.json(data);
+        });
+    });
+
+    // GET route for retrieving single recipe
+    app.get("/api/recipes/:recipe_name", function(req,res){
+        db.Recipe.findOne({
+            where: {
+                recipe_name: req.params.recipe_name
+            }
+        })
+        .then(function(data){
+            res.json(data);
+        });
+    })
+    // POST route for saving a new recipe
+    app.post("api/recipes",function(req,res){
+        db.Recipe.create({
+            user_name: req.body.user_name,
+            recipe_name:req.body.recipe_name,
+            ingredients:req.body.ingredients,
+            instructions:req.body.instructions,
+            cook_time:req.body.cook_time,
+            prep_time:req.body.prep_time
+        })
+    })
+
+    }
