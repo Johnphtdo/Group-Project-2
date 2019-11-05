797ad679826a7eb62a2f0655086faa991a87463a
Dohn Joe <johnphtdo@gmail.com>
2019-11-05 12:51:50 -0600

    added seed data and removed the drop database from server.js

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO recipes(user_name,recipe_name,ingredients,instructions,cook_time,prep_time)
+
+VALUES ("JohnCANTcode", "Ice", "1 Cup(s) Water", "Boil the water, pour it into an ice tray, and place into the freezer", "60","5")
